cac703a2f007051891b42e2a86eed155d8152c88
Sergio León <panicoenlaxbox@gmail.com>
2022-11-14 16:58:50 +0100

    Sphinx autodoc

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. mediatpy documentation master file, created by
+   sphinx-quickstart on Mon Nov 14 14:44:52 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to mediatpy's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: mediatpy
+   :members:
+   :undoc-members:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/mediatpy/__init__.py
+++ src/mediatpy/__init__.py
@@ -7,6 +7,10 @@ TResponse = TypeVar("TResponse")
 
 
 class Request(Generic[TResponse]):
+    """
+    Base class for any request
+    """
+
     pass
 
 
@@ -14,18 +18,36 @@ TRequest = TypeVar("TRequest", bound=Request)
 
 
 class RequestHandler(ABC, Generic[TRequest, TResponse]):
+    """
+    Base class for any request handler
+    """
+
     @abstractmethod
     async def handle(self, request: TRequest) -> TResponse:
         pass
 
 
 class PipelineBehavior(ABC, Generic[TRequest, TResponse]):
+    """
+    Base class for any pipeline behavior
+    """
+
     @abstractmethod
     async def handle(self, request: TRequest, next_behavior: Callable[..., Awaitable[TResponse]]) -> TResponse:
+        """
+        Abstract method to handle a request and to return a response
+
+        :param request: Request to be handled
+        :param next_behavior: Next pipeline behavior or request handler to execute
+        """
         pass
 
 
 class Notification:
+    """
+    Base class for any notification
+    """
+
     pass
 
 
@@ -33,6 +55,10 @@ TNotification = TypeVar("TNotification", bound=Notification)
 
 
 class NotificationHandler(ABC, Generic[TNotification]):
+    """
+    Base class for any notification handler
+    """
+
     @abstractmethod
     async def handle(self, notification: TNotification) -> None:
         pass
@@ -42,24 +68,42 @@ Handler: TypeAlias = Type[RequestHandler | PipelineBehavior | NotificationHandle
 
 
 @dataclass
-class PipelineBehaviorRegistration:
+class _PipelineBehaviorRegistration:
     pipeline_behavior: Type[PipelineBehavior]
     position: int
 
 
 class NoRequestHandlerFoundError(Exception):
+    """
+    Error to indicate that a :class:`Request` has not a registered :class:`RequestHandler` to handle it
+    """
+
     def __init__(self, request: Request) -> None:
         self.request = request
         super().__init__(f"There is no registered handler for request {type(request)}.")
 
 
 class NotAnyNotificationHandlerFoundError(Exception):
+    """
+    Error to indicate that a :class:`Notification` has not any registered :class:`NotificationHandler` to handle it
+    """
+
     def __init__(self, notification: Notification) -> None:
         self.notification = notification
         super().__init__(f"There are not any registered handler for notification {notification}.")
 
 
 class Mediator:
+    """
+    Class to register handlers and coordinate their execution in response to arrivals of requests and notifications
+
+    :param request_handler_factory: Custom :class:`RequestHandler` factory
+    :param pipeline_behavior_factory: Custom :class:`PipelineBehavior` factory
+    :param notification_handler_factory: Custom :class:`NotificationHandler` factory
+    :param raise_error_if_not_any_registered_notification_handler: Raise an error if no :class:`NotificationHandler`
+        is found when a :class:`Notification` is published
+    """
+
     def __init__(
         self,
         request_handler_factory: Optional[Callable[[Type[RequestHandler]], RequestHandler]] = None,
@@ -74,7 +118,7 @@ class Mediator:
         self._pipeline_behavior_factory = (
             self._default_pipeline_behavior_factory if pipeline_behavior_factory is None else pipeline_behavior_factory
         )
-        self._pipeline_behaviors: dict[Type[Request], list[PipelineBehaviorRegistration]] = {}
+        self._pipeline_behaviors: dict[Type[Request], list[_PipelineBehaviorRegistration]] = {}
         self._pipeline_behavior_position = 0
         self._notification_handler_factory = (
             self._default_notification_handler_factory
@@ -107,32 +151,58 @@ class Mediator:
         return get_args(notification_handler.__orig_bases__[0])[0]  # type: ignore
 
     def request_handler(self, request_handler: Type[RequestHandler]) -> None:
+        """
+        Decorator to register a :class:`RequestHandler`
+        """
         self.register_request_handler(request_handler)
 
     def pipeline_behavior(self, pipeline_behavior: Type[PipelineBehavior]) -> None:
+        """
+        Decorator to register a :class:`PipelineBehavior`
+        """
         self.register_pipeline_behavior(pipeline_behavior)
 
     def notification_handler(self, notification_handler: Type[NotificationHandler]) -> None:
+        """
+        Decorator to register a :class:`NotificationHandler`
+        """
         self.register_notification_handler(notification_handler)
 
     def register_request_handler(self, request_handler: Type[RequestHandler]) -> None:
+        """
+        Manual registration of a :class:`RequestHandler`
+        """
         request = self._get_request_type(request_handler)
         self._request_handlers[request] = request_handler
 
     def register_notification_handler(self, notification_handler: Type[NotificationHandler]) -> None:
+        """
+        Manual registration of a :class:`NotificationHandler`
+        """
         notification = self._get_notification_type(notification_handler)
         notification_handlers = self._notification_handlers.get(notification, [])
         notification_handlers.append(notification_handler)
         self._notification_handlers[notification] = notification_handlers
 
     def register_pipeline_behavior(self, pipeline_behavior: Type[PipelineBehavior]) -> None:
+        """
+        Manual registration of a :class:`PipelineBehavior`
+        """
         request = self._get_request_type(pipeline_behavior)
         pipeline_behaviors = self._pipeline_behaviors.get(request, [])
-        pipeline_behaviors.append(PipelineBehaviorRegistration(pipeline_behavior, self._pipeline_behavior_position))
+        pipeline_behaviors.append(_PipelineBehaviorRegistration(pipeline_behavior, self._pipeline_behavior_position))
         self._pipeline_behaviors[request] = pipeline_behaviors
         self._pipeline_behavior_position += 1
 
     async def send(self, request: Request[TResponse]) -> TResponse:
+        """
+        Send a :class:`Request`
+
+        .. code-block:: python
+
+            my_request = MyRequest()
+            await mediator.send(my_request)
+        """
         if not type(request) in self._request_handlers:
             raise NoRequestHandlerFoundError(request)
         request_handler = self._create_request_handler(self._request_handlers[type(request)])
@@ -146,6 +216,14 @@ class Mediator:
         return await first_pipeline_behavior.handle(request, next_pipeline_behavior)
 
     async def publish(self, notification: Notification) -> None:
+        """
+        Publish a :class:`Notification`
+
+        .. code-block:: python
+
+            my_notification = MyNotification()
+            await mediator.publish(my_notification)
+        """
         notification_handlers = self._resolve_notification_handlers(notification)
         if not any(notification_handlers) and self._raise_error_if_not_any_registered_notification_handler:
             raise NotAnyNotificationHandlerFoundError(notification)
@@ -163,7 +241,7 @@ class Mediator:
         return self._notification_handler_factory(notification_handler)
 
     @cache
-    def _resolve_pipeline_behaviors(self, request: Request) -> list[PipelineBehaviorRegistration]:
+    def _resolve_pipeline_behaviors(self, request: Request) -> list[_PipelineBehaviorRegistration]:
         matching = [value for key, value in self._pipeline_behaviors.items() if issubclass(type(request), key)]
         flattened = [pipeline_behavior for sublist in matching for pipeline_behavior in sublist]
         return sorted(flattened, key=lambda pipeline_behavior: pipeline_behavior.position)
@@ -177,7 +255,7 @@ class Mediator:
         self,
         request: Request,
         request_handler: RequestHandler,
-        pipeline_behaviors: list[PipelineBehaviorRegistration],
+        pipeline_behaviors: list[_PipelineBehaviorRegistration],
         index: int,
     ) -> Callable[..., Awaitable[TResponse]]:
         def _() -> Coroutine:
